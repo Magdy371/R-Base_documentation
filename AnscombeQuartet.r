dda0afd76b78c4152b0d22e708181596d40111c0
Magdy Elshrief <73464211+Magdy371@users.noreply.github.com>
2023-02-13 05:16:55 +0000

    importance_of_Data_viz-3
    in this file we learn about the importance of data visualization
    an Anscombe quartet has 4 data sets which may be misleading in their out comes of the (mean , standard deviation and Correlation)
    so we need data visualization to show the diference in these four data sets

---
+++ AnscombeQuartet.r
@@ -0,0 +1,12 @@
+library(Tmisc)
+library(datasauRus)
+data(quartet)
+#View(quartet)
+#skim_without_charts(quartet)
+quartet %>%
+  group_by(set)%>%
+  summarize(mean_x = mean(x) , standardeviation_X = sd(x),mean_y = mean(y),
+            standardeviation_Y = sd(y),cor(x,y))
+ggplot(quartet, aes(x,y))+ geom_point() + 
+  geom_smooth(method=lm,se = FALSE)+facet_wrap(~set)
+# Data sauRus creates plots with Anscombe data in different shapes
